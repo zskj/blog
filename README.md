b8119e84e85896d24d8e98636bcfe36e78536507
zskjali <zskjcn@qq.com>
2019-12-18 09:22:10 +0800

    readme

---
+++ README.md
@@ -0,0 +1,61 @@
+### Required
+- go version 1.13.4
+- mysql
+
+###Ready
+Create a **blog database** and import [SQL](https://github.com/zskj/blog/blob/master/docs/sql/blog.sql)
+
+### Conf
+
+You should modify `conf/app.ini`
+
+```
+[database]
+Type = mysql
+User = root
+Password =
+Host = 127.0.0.1:3306
+Name = blog
+TablePrefix = blog_
+```
+### Install
+```
+$ git clone https://github.com/zskj/blog.git
+$ cd $path/blog
+$ go mod tidy
+$ go mod vendor
+$ swag init
+$ go run main.go
+```
+### Project information and existing API
+```
+[GIN-debug] [WARNING] Running in "debug" mode. Switch to "release" mode in production.
+ - using env:   export GIN_MODE=release
+ - using code:  gin.SetMode(gin.ReleaseMode)
+
+[GIN-debug] POST   /auth                     --> blog/routers/api.Auth (4 handlers)
+[GIN-debug] GET    /swagger/*any             --> github.com/swaggo/gin-swagger.CustomWrapHandler.func1 (4 handlers)
+[GIN-debug] GET    /api/v1/tags              --> blog/routers/api/v1.GetTags (5 handlers)
+[GIN-debug] POST   /api/v1/tags              --> blog/routers/api/v1.AddTag (5 handlers)
+[GIN-debug] PUT    /api/v1/tags/:id          --> blog/routers/api/v1.EditTag (5 handlers)
+[GIN-debug] DELETE /api/v1/tags/:id          --> blog/routers/api/v1.DeleteTag (5 handlers)
+[GIN-debug] GET    /api/v1/articles          --> blog/routers/api/v1.GetArticles (5 handlers)
+[GIN-debug] GET    /api/v1/articles/:id      --> blog/routers/api/v1.GetArticle (5 handlers)
+[GIN-debug] POST   /api/v1/articles          --> blog/routers/api/v1.AddArticle (5 handlers)
+[GIN-debug] PUT    /api/v1/articles/:id      --> blog/routers/api/v1.EditArticle (5 handlers)
+[GIN-debug] DELETE /api/v1/articles/:id      --> blog/routers/api/v1.DeleteArticle (5 handlers)
+[GIN-debug] GET    /test                     --> blog/routers.InitRouter.func1 (4 handlers)
+
+```
+### Swaggo
+
+> http://127.0.0.1:8000/swagger/index.html
+
+![demo](docs/screenshots/sewagger.png)
+
+
+
+
+
+
+
---
+++ docs/sql/blog.sql
@@ -0,0 +1,83 @@
+/*
+Navicat MySQL Data Transfer
+
+Source Server         : 本地
+Source Server Version : 50717
+Source Host           : localhost:3306
+Source Database       : blog
+
+Target Server Type    : MYSQL
+Target Server Version : 50717
+File Encoding         : 65001
+
+Date: 2019-12-18 09:01:05
+*/
+
+SET FOREIGN_KEY_CHECKS=0;
+
+-- ----------------------------
+-- Table structure for blog_article
+-- ----------------------------
+DROP TABLE IF EXISTS `blog_article`;
+CREATE TABLE `blog_article` (
+  `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
+  `tag_id` int(10) unsigned DEFAULT '0' COMMENT '标签ID',
+  `title` varchar(100) DEFAULT '' COMMENT '文章标题',
+  `desc` varchar(255) DEFAULT '' COMMENT '简述',
+  `content` text,
+  `created_on` int(11) DEFAULT NULL,
+  `created_by` varchar(100) DEFAULT '' COMMENT '创建人',
+  `modified_on` int(10) unsigned DEFAULT '0' COMMENT '修改时间',
+  `modified_by` varchar(255) DEFAULT '' COMMENT '修改人',
+  `deleted_on` int(10) unsigned DEFAULT '0',
+  `state` tinyint(3) unsigned DEFAULT '1' COMMENT '状态 0为禁用1为启用',
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8 COMMENT='文章管理';
+
+-- ----------------------------
+-- Records of blog_article
+-- ----------------------------
+INSERT INTO `blog_article` VALUES ('1', '1', 'test-edit1', 'test-desc-edit', 'test-content-edit', '1575945565', 'test-created', '1575945829', 'test-created-edit', '0', '1');
+
+-- ----------------------------
+-- Table structure for blog_tag
+-- ----------------------------
+DROP TABLE IF EXISTS `blog_tag`;
+CREATE TABLE `blog_tag` (
+  `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
+  `name` varchar(100) DEFAULT '' COMMENT '标签名称',
+  `created_on` int(10) unsigned DEFAULT '0' COMMENT '创建时间',
+  `created_by` varchar(100) DEFAULT '' COMMENT '创建人',
+  `modified_on` int(10) unsigned DEFAULT '0' COMMENT '修改时间',
+  `modified_by` varchar(100) DEFAULT '' COMMENT '修改人',
+  `deleted_on` int(10) unsigned DEFAULT '0',
+  `state` tinyint(3) unsigned DEFAULT '1' COMMENT '状态 0为禁用、1为启用',
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=7 DEFAULT CHARSET=utf8 COMMENT='文章标签管理';
+
+-- ----------------------------
+-- Records of blog_tag
+-- ----------------------------
+INSERT INTO `blog_tag` VALUES ('1', '我们', '1575937993', 'test', '0', '', '0', '1');
+INSERT INTO `blog_tag` VALUES ('4', '我', '1575940902', 'test', '0', '', '0', '0');
+INSERT INTO `blog_tag` VALUES ('5', 'pp', '1575940931', 'test', '0', '', '0', '0');
+INSERT INTO `blog_tag` VALUES ('6', 'tu', '1575967899', 'tu', '0', '', '0', '1');
+
+-- ----------------------------
+-- Table structure for blog_user
+-- ----------------------------
+DROP TABLE IF EXISTS `blog_user`;
+CREATE TABLE `blog_user` (
+  `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
+  `username` varchar(50) DEFAULT '' COMMENT '账号',
+  `password` varchar(50) DEFAULT '' COMMENT '密码',
+  `created_on` int(11) unsigned DEFAULT NULL COMMENT '创建时间',
+  `modified_on` int(11) unsigned DEFAULT NULL COMMENT '更新时间',
+  `deleted_on` int(11) unsigned DEFAULT '0' COMMENT '删除时间戳',
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8 COMMENT='用户管理';
+
+-- ----------------------------
+-- Records of blog_user
+-- ----------------------------
+INSERT INTO `blog_user` VALUES ('1', 'admin', 'e10adc3949ba59abbe56e057f20f883e', '0', '0', '0');
