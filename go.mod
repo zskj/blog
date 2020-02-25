b2a8f7198e431aabcd6039acb1dbfad30dcef801
zskj <zskjcn@qq.com>
2020-02-25 15:59:03 +0800

    fix version

--- go.mod
+++ go.mod
@@ -3,27 +3,19 @@ module blog
 go 1.13
 
 require (
-	github.com/Bowery/prompt v0.0.0-20190916142128-fa8279994f75 // indirect
 	github.com/alecthomas/template v0.0.0-20190718012654-fb15b899a751
 	github.com/astaxie/beego v1.12.0
-	github.com/caibirdme/gforge v0.0.0-20180821021955-5b28ef91cac0 // indirect
 	github.com/dchest/captcha v0.0.0-20170622155422-6a29415a8364
 	github.com/dgrijalva/jwt-go v3.2.0+incompatible
-	github.com/didi/gendry v1.3.1 // indirect
-	github.com/gin-gonic/gin v1.5.0
+	github.com/gin-gonic/gin v1.4.0
 	github.com/go-ini/ini v1.51.0
 	github.com/go-sql-driver/mysql v1.5.0 // indirect
 	github.com/jinzhu/gorm v1.9.11
-	github.com/labstack/gommon v0.3.0 // indirect
-	github.com/mattn/go-colorable v0.1.4 // indirect
 	github.com/mattn/go-isatty v0.0.11 // indirect
-	github.com/mkideal/cli v0.0.3 // indirect
-	github.com/mkideal/pkg v0.0.0-20170503154153-3e188c9e7ecc // indirect
 	github.com/stretchr/testify v1.4.0
 	github.com/swaggo/gin-swagger v1.2.0
-	github.com/swaggo/swag v1.6.3
+	github.com/swaggo/swag v1.6.5
 	github.com/unknwon/com v1.0.1
 	golang.org/x/sys v0.0.0-20200107162124-548cf772de50 // indirect
-	google.golang.org/appengine v1.6.5 // indirect
 	gopkg.in/ini.v1 v1.51.0 // indirect
 )
--- models/tag.go
+++ models/tag.go
@@ -7,51 +7,50 @@ import (
 
 type Tag struct {
 	Model
-	Name string `json:"name"`
-	CreatedBy string `json:"created_by"`
+	Name       string `json:"name"`
+	CreatedBy  string `json:"created_by"`
 	ModifiedBy string `json:"modified_by"`
-	State int `json:"state"`
+	State      int    `json:"state"`
 }
 
 func (tag *Tag) BeforeCreate(scope *gorm.Scope) error {
 	scope.SetColumn("CreatedOn", time.Now().Unix())
-
 	return nil
 }
 
 /**
 gorm 的Callbacks
 
- */
+*/
 func (tag *Tag) BeforeUpdate(scope *gorm.Scope) error {
 	scope.SetColumn("ModifiedOn", time.Now().Unix())
 
 	return nil
 }
 
-func GetTags(pageNum int ,pageSize int ,maps interface{}) (tags []Tag){
+func GetTags(pageNum int, pageSize int, maps interface{}) (tags []Tag) {
 	db.Where(maps).Offset(pageNum).Limit(pageSize).Find(&tags)
 	return
 }
 
-func GetTagTotal(maps interface{})(count int){
+func GetTagTotal(maps interface{}) (count int) {
 	db.Model(&Tag{}).Where(maps).Count(&count)
 	return
 }
 
-func ExistTagByName(name string) bool{
+func ExistTagByName(name string) bool {
 	var tag Tag
-	db.Select("id").Where("name = ?",name).First(&tag)
-	if(tag.ID > 0){
+	db.Select("id").Where("name = ?", name).First(&tag)
+	if tag.ID > 0 {
 		return true
 	}
 	return false
 }
 
-func AddTag(name string ,state int ,createdBy string) bool{
-	db.Create(&Tag {
-		Name : name,
-		State : state,
+func AddTag(name string, state int, createdBy string) bool {
+	db.Create(&Tag{
+		Name:      name,
+		State:     state,
 		CreatedBy: createdBy,
 	})
 
@@ -68,7 +67,7 @@ func ExistTagByID(id int) bool {
 	return false
 }
 
-func EditTag(id int, data interface {}) bool {
+func EditTag(id int, data interface{}) bool {
 	db.Model(&Tag{}).Where("id = ?", id).Updates(data)
 
 	return true
